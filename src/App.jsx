fd7b844376f07c4ffe3c86e704b1c54a8deca03c
Pooja Sharma <poojasharma09740@gmail.com>
2023-12-15 20:29:19 +0530

    changes done in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './components/Home'
 import Navbar from './components/Navbar'
 import Projects from './components/Projects'
 import Skills from './components/Skills'
-import Contact from './components/contact'
+import Contact from './components/Contact'
 
 function App() {
   return (
