066bef698cecc020eeab23eb841ddf300a88b86d
Pooja Sharma <poojasharma09740@gmail.com>
2023-12-15 16:13:00 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import About from './components/About'
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import Projects from './components/Projects'
+import Skills from './components/Skills'
+import Contact from './components/contact'
+
+function App() {
+  return (
+    <>
+      <main>
+        <Navbar/>
+        <Home />
+        <About/>
+        <Skills /> 
+        <Projects />
+        <Contact />
+      </main> 
+    </>
+  )
+}
+
+export default App
+
+// Tamal Sen -  https://tamalsen.dev/
+// Ram Maheshwari - https://www.rammaheshwari.com/
+// Matt Farley - https://mattfarley.ca/
