c4b7188eada285b99dffe3eee65b229b6dc5f544
Pooja Sharma <poojasharma09740@gmail.com>
2024-07-04 17:13:27 +0530

    Added new projects

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,84 @@
 export default function Home() {
+  var TxtRotate = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+  };
+  
+  TxtRotate.prototype.tick = function() {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+  
+    if (this.isDeleting) {
+      this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+      this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+  
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  
+    var that = this;
+    var delta = 300 - Math.random() * 100;
+  
+    if (this.isDeleting) { delta /= 2; }
+  
+    if (!this.isDeleting && this.txt === fullTxt) {
+      delta = this.period;
+      this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+      this.isDeleting = false;
+      this.loopNum++;
+      delta = 500;
+    }
+  
+    setTimeout(function() {
+      that.tick();
+    }, delta);
+  };
+  
+  window.onload = function() {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i=0; i<elements.length; i++) {
+      var toRotate = elements[i].getAttribute('data-rotate');
+      var period = elements[i].getAttribute('data-period');
+      if (toRotate) {
+        new TxtRotate(elements[i], JSON.parse(toRotate), period);
+      }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #7843e9 }";
+    document.body.appendChild(css);
+  };
+  
   return (
     <div>
-    <div className="home min-h-screen flex flex-col gap-10">
-      <div className="homeHeading">HEY, I&apos;M POOJA SHARMA</div>
-      <div className="homeSubHeading">Aspiring Software Engineer & Web Developer</div>
-      <div className="hometag">Transforming lines of code into meaningful experiences.</div>
-    </div>
-    <svg className="arrows">
-              <path className="a1" d="M0 0 L30 32 L60 0"></path>
-              <path className="a2" d="M0 20 L30 52 L60 20"></path>
-              <path className="a3" d="M0 40 L30 72 L60 40"></path>
-            </svg>
+      <div className="home min-h-screen flex flex-col gap-10">
+        <div className="homeHeading">
+          HEY, I&apos;M{" "}
+          <span
+            className="txt-rotate"
+            data-period="2000"
+            data-rotate='[ " POOJA SHARMA.", " A DEVELOPER.", " A LEARNER." ]'
+          ></span>
+        </div>
+        <div className="homeSubHeading">
+          Aspiring Software Engineer & Web Developer
+        </div>
+        <div className="hometag">
+          Transforming lines of code into meaningful experiences.
+        </div>
+      </div>
+      <svg className="arrows">
+        <path className="a1" d="M0 0 L30 32 L60 0"></path>
+        <path className="a2" d="M0 20 L30 52 L60 20"></path>
+        <path className="a3" d="M0 40 L30 72 L60 40"></path>
+      </svg>
     </div>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,142 @@ import FadeInSection from "./FadeIn";
 export default function Projects() {
   return (
     <FadeInSection>
-      <div className="about" id="projects">
+      <div id="projects">
+        <h1 className="projectHeading">MY PROJECTS</h1>
+        <div className="container">
+          <div className="card">
+            <img src="https://images.unsplash.com/photo-1604480132736-44c188fe4d20?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8bWVudGFsJTIwaGVhbHRofGVufDB8fDB8fHww" />
+            <div className="info">
+              <h1 className="cardheading">InnerHealer</h1>
+              <p className="cardtech">ReactJS, NodeJS, ExpressJS, MongoDB</p>
+              <form>
+                <button
+                  className="cardbutton"
+                  type="submit"
+                  formAction="https://github.com/poojasharma097/InnerHealer"
+                  formTarget="_blank"
+                >
+                  Code
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="card">
+            <img src="https://images.unsplash.com/photo-1496417263034-38ec4f0b665a?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            <div className="info">
+              <h1 className="cardheading">HotelHaven</h1>
+              <p className="cardtech">ReactJS, NodeJS, ExpressJS, MongoDB</p>
+              <form>
+                <button
+                  className="cardbutton"
+                  type="submit"
+                  formAction="https://github.com/poojasharma097/HotelHaven"
+                  formTarget="_blank"
+                >
+                  Code
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="card">
+            <img src="https://images.unsplash.com/photo-1456324504439-367cee3b3c32?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            <div className="info">
+              <h1>To-Do List</h1>
+              <p>HTML, CSS, Javascript, NodeJS, ExpressJS, MongoDB</p>
+              <form>
+                <button
+                  type="submit"
+                  formAction="https://github.com/poojasharma097/To-Do-List"
+                  formTarget="_blank"
+                >
+                  Code
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="card">
+            <img src="https://images.unsplash.com/photo-1511871893393-82e9c16b81e3?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
+            <div className="info">
+              <h1>Diary</h1>
+              <p>HTML, CSS, Javascript, NodeJS, ExpressJS, MongoDB</p>
+              <form>
+                <button
+                  type="submit"
+                  formAction="https://github.com/poojasharma097/Blog-Website"
+                  formTarget="_blank"
+                >
+                  Code
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="card">
+            <img src="https://images.unsplash.com/photo-1553991562-9f24b119ff51?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8c2VjcmV0fGVufDB8fDB8fHww" />
+            <div className="info">
+              <h1>Secrets</h1>
+              <p>HTML, CSS, Javascript, NodeJS, ExpressJS, MongoDB</p>
+              <form>
+                <button
+                  type="submit"
+                  formAction="https://github.com/poojasharma097/Secrets"
+                  formTarget="_blank"
+                >
+                  Code
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="card">
+            <img src="https://images.unsplash.com/photo-1525909002-1b05e0c869d8?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGNvbG91cnxlbnwwfHwwfHx8MA%3D%3D" />
+            <div className="info">
+              <h1>Simon Game</h1>
+              <p>Flutter, Dart</p>
+              <form>
+                <button
+                  type="submit"
+                  formAction="https://github.com/poojasharma097/Simon-Game"
+                  formTarget="_blank"
+                >
+                  Code
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="card">
+            <img src="https://images.unsplash.com/photo-1556695736-d287caebc48e?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bWV0cm98ZW58MHx8MHx8fDA%3D" />
+            <div className="info">
+              <h1>MetAssistant</h1>
+              <p>C++</p>
+              <form>
+                <button
+                  type="submit"
+                  formAction="https://github.com/poojasharma097/MetAssistant"
+                  formTarget="_blank"
+                >
+                  Code
+                </button>
+              </form>
+            </div>
+          </div>
+          <div className="card">
+            <img src="https://images.unsplash.com/photo-1511882150382-421056c89033?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8YXJjYWRlfGVufDB8fDB8fHww" />
+            <div className="info">
+              <h1>Arcade</h1>
+              <p>C++</p>
+              <form>
+                <button
+                  type="submit"
+                  formAction="https://github.com/poojasharma097/Arcade"
+                  formTarget="_blank"
+                >
+                  Code
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* <div className="about" id="projects">
         <h1 className="aboutHeading">MY PROJECTS</h1>
         <div className="container">
           <div className="project p1">
@@ -22,104 +157,8 @@ export default function Projects() {
               </button>
             </form>
           </div>
-          <div className="project p2">
-            <div>
-              <h2>To-Do List</h2>
-              <h3 className="projectdesc">Web Development</h3>
-            </div>
-            <form>
-              <button
-                className="projectbutton"
-                type="submit"
-                formAction="https://github.com/poojasharma097/To-Do-List"
-                formTarget="_blank"
-              >
-                View Project
-              </button>
-            </form>
-          </div>
-          <div className="project p3">
-            <div>
-              <h2>Diary</h2>
-              <h3 className="projectdesc">Web Development</h3>
-            </div>
-            <form>
-              <button
-                className="projectbutton"
-                type="submit"
-                formAction="https://github.com/poojasharma097/Blog-Website"
-                formTarget="_blank"
-              >
-                View Project
-              </button>
-            </form>
-          </div>
-          <div className="project p4">
-            <div>
-              <h2>Secrets</h2>
-              <h3 className="projectdesc">Web Development</h3>
-            </div>
-            <form>
-              <button
-                className="projectbutton"
-                type="submit"
-                formAction="https://github.com/poojasharma097/Secrets"
-                formTarget="_blank"
-              >
-                View Project
-              </button>
-            </form>
-          </div>
-          <div className="project p5">
-            <div>
-              <h2>Simon Game</h2>
-              <h3 className="projectdesc">App Development</h3>
-            </div>
-            <form>
-              <button
-                className="projectbutton"
-                type="submit"
-                formAction="https://github.com/poojasharma097/Simon-Game"
-                formTarget="_blank"
-              >
-                View Project
-              </button>
-            </form>
-          </div>
-          <div className="project p6">
-            <div>
-              <h2>MetAssistant</h2>
-              <h3 className="projectdesc">C++</h3>
-            </div>
-            <form>
-              <button
-                className="projectbutton"
-                type="submit"
-                formAction="https://github.com/poojasharma097/MetAssistant"
-                formTarget="_blank"
-              >
-                View Project
-              </button>
-            </form>
-          </div>
-          <div className="project p7">
-            <div>
-              <h2>Arcade</h2>
-              <h3 className="projectdesc">C++</h3>
-            </div>
-            <form>
-              <button
-                className="projectbutton"
-                type="submit"
-                formAction="https://github.com/poojasharma097/Arcade"
-                formTarget="_blank"
-              >
-                View Project
-              </button>
-            </form>
-          </div>
         </div>
-      </div>
+      </div> */}
     </FadeInSection>
   );
 }
